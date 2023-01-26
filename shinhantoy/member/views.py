a42e80faa8cb9c09849ba7276e8babcbaf0f2e97
yyueun <sw3623@ajou.ac.kr>
2023-01-26 13:55:22 +0900

    feat: Made change_password.html

--- shinhantoy/member/views.py
+++ shinhantoy/member/views.py
@@ -5,6 +5,8 @@ from rest_framework import mixins, generics, status
 from rest_framework.response import Response 
 from rest_framework.views import APIView
 from django.contrib.auth.hashers import check_password, make_password
+from rest_framework.permissions import IsAuthenticated
+
 
 #signup 
 class RegisterView(
@@ -22,8 +24,10 @@ class RegisterView(
 
 
 class ChangePW(APIView):
+    permission_classes = [IsAuthenticated]
+
+
     def put(self,request,*args,**kwargs):
-        username = request.data.get('username')
         password = request.data.get('password')
         change_password = request.data.get('change_password') #변경 비밀번호
         change_password2 = request.data.get('change_password2') #변경 비밀번호 한번 더
@@ -32,16 +36,8 @@ class ChangePW(APIView):
             return Response({
                 'detail':'Wrong new password'
             },status=status.HTTP_400_BAD_REQUEST)
-    #step2 유저네임이 없으면 계정없다고 반환함 
-        if not Member.objects.get(username=username):
-            return Response({
-                'detail':'No accout'
-            },status=status.HTTP_404_NOT_FOUND)
- 
-    #step3 member.objects.get(유저네임=유저네임) 유저네임가져오기
 
-        member = Member.objects.get(uername=username)
-
+        member = request.user
     #step4 checkpassword 함수를 사용해서 암호호된 비밀번호와 맞는지 확인하기 
         if  not check_password(password,member.password):  #패스워드랑 멤버의 있는 패스워드랑 같은지 확인하기 -> 일치하지않으면
             return Response({
